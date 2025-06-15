fb123d973e2f0f57f0e44a0dabbfbf231990dad5
Robin Heggelund Hansen <git@heghan.org>
2025-06-15 08:46:42 +0200

    Begin working on script.

---
+++ index.js
@@ -0,0 +1,129 @@
+#!/usr/bin/env node
+
+const fs = require("fs/promises");
+const cp = require("child_process");
+const postject = require("postject");
+
+const isMac = process.platform === "darwin";
+const isWin = process.platform === "win32";
+
+const nodePath = process.execPath;
+const args = process.argv.slice(2);
+const cliOpts = args.filter((arg) => arg.startsWith("-"));
+const cliArgs = args.filter((arg) => !arg.startsWith("-"));
+
+if (cliOpts.includes("-h") || cliOpts.includes("--help")) {
+  console.log("help");
+} else if (cliArgs.length !== 1) {
+  console.log("help");
+} else if (cliOpts.includes("-s") || cliOpts.includes("--snapshot")) {
+  console.log("snapshot");
+} else {
+  console.log("executable");
+}
+
+async function makeSnapshot(output) {
+  const jsBuildPath = output + ".tmp";
+  const blobPath = output + ".blob";
+
+  try {
+    await makeSnapshotCompatible(output, jsBuildPath);
+
+    // Generate the snapshot
+    cp.execFileSync(nodePath, [
+      "--snapshot-blob-path",
+      blobPath,
+      "--build-snapshot",
+      jsBuildPath,
+    ]);
+
+    console.log("Done!");
+  } catch (e) {
+    console.error("Failed to create snapshot", e);
+  } finally {
+    // cleanup
+    await fs.rm(jsBuildPath);
+  }
+}
+
+// For snapshots to work we need to wrap the function call that starts
+// the Gren application, with a hint that tells the V8 engine what the
+// main function is
+async function makeSnapshotCompatible(input, target) {
+  const compiledSrc = await fs.readFile(input, "utf-8");
+
+  const initRegex = /this\.Gren\..+\(\{\}\);/g;
+  const initCall = compiledSrc.match(initRegex)[0];
+
+  const snapshotCompatibleSrc = compiledSrc.replace(
+    initCall,
+    `
+  const v8 = require('node:v8');
+  v8.startupSnapshot.setDeserializeMainFunction(function() {
+    ${initCall}
+  });
+  `,
+  );
+
+  await fs.writeFile(jsBuildPath, snapshotCompatibleSrc);
+}
+
+async function makeExecutable(output) {
+  const jsBuildPath = output + ".tmp";
+  const blobPath = output + ".blob";
+
+  const seaConfigPath = output + ".sea.config";
+  const seaConfig = {
+    main: jsBuildPath,
+    output: blobPath,
+    disableExperimentalSEAWarning: true,
+    useSnapshot: true,
+  };
+
+  const binPath = isWin ? output + ".node.exe" : output + ".node";
+
+  try {
+    await makeSnapshotCompatible(output, jsBuildPath);
+
+    // Generate the snapshot
+    await fs.writeFile(seaConfigPath, JSON.stringify(seaConfig));
+    cp.execFileSync(nodePath, ["--experimental-sea-config", seaConfigPath]);
+
+    // Then copy the node executable and inject the snapshot into it
+    await fs.copyFile(nodePath, binPath);
+    await fs.chmod(binPath, "755");
+
+    if (isMac) {
+      // required on mac, optional on windows, not required on linux
+      cp.execFileSync("codesign", ["--remove-signature", binPath]);
+    }
+
+    const blobContent = await fs.readFile(blobPath);
+
+    await postject.inject(binPath, "NODE_SEA_BLOB", blobContent, {
+      sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2",
+      machoSegmentName: isMac ? "NODE_SEA" : undefined,
+    });
+
+    if (isMac) {
+      // required on mac
+      cp.execFileSync("codesign", ["--sign", "-", binPath]);
+    }
+
+    const outputPath = isWin ? jsBuildPath + ".exe" : jsBuildPath;
+    await fs.rename(binPath, outputPath);
+
+    if (isWin) {
+      await fs.rm(jsBuildPath);
+    }
+
+    console.log("Done!");
+  } catch (e) {
+    console.error("Failed to create static executable", e);
+  } finally {
+    // cleanup
+    await fs.rm(jsBuildPath);
+    await fs.rm(seaConfigPath);
+    await fs.rm(blobPath);
+  }
+}
